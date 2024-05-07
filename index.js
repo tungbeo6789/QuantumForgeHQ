285f53d4b5aaf551a7c2e8a2d3c89d4649d1f041
tungbeo6789 <lschroder7nam1311@outlook.com>
2024-05-07 23:52:24 +0700

    test task

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-function canCross(stones) {
-  const map = new Map();
-  for (const stone of stones) map.set(stone, new Set());
-  map.get(0).add(0);
-  for (let i = 0; i < stones.length; i++) {
-    for (const step of map.get(stones[i])) {
-      for (let k = step - 1; k <= step + 1; k++) {
-        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
-      }
-    }
+const factorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
   }
-  return map.get(stones[stones.length - 1]).size > 0;
-}
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
