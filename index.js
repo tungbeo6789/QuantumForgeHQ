1038e96bda6c9dce351e2b4b895a7353550e33df
tungbeo6789 <lschroder7nam1311@outlook.com>
2024-05-14 21:08:08 +0700

    feat task

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-function minDepth(root) {
-  if (!root) return 0;
-  if (!root.left && !root.right) return 1;
-  let min = Infinity;
-  if (root.left) min = Math.min(min, minDepth(root.left));
-  if (root.right) min = Math.min(min, minDepth(root.right));
-  return min + 1;
+function greet(name) {
+  console.log("Hello, " + name + "!");
 }
+
+greet("Bob");
