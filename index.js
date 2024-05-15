9d97fce78a77e6efa4c21e308b91aa173acca714
tungbeo6789 <lschroder7nam1311@outlook.com>
2024-05-15 22:50:54 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function greet(name) {
-  console.log("Hello, " + name + "!");
+function canFinish(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  for (const [course, pre] of prerequisites) {
+    graph[pre].push(course);
+    inDegree[course]++;
+  }
+  const queue = [];
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) queue.push(i);
+  }
+  let count = 0;
+  while (queue.length) {
+    const course = queue.shift();
+    count++;
+    for (const nextCourse of graph[course]) {
+      inDegree[nextCourse]--;
+      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
+    }
+  }
+  return count === numCourses;
 }
-
-greet("Bob");
